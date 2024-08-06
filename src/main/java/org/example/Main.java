1e6ad2d9aeb63a11365ffa265a5c33c06799cd3a
mozarslan34 <mozarslan@outlook.com>
2024-08-07 00:32:26 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * @author Mertcan Özarslan
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
